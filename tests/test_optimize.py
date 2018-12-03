c4f77bde5143f00e69f5cb92a6c92c2aaebbd1ea
Sara Yueting Luo <yluo@hsph.harvard.edu>
2018-12-03 12:02:59 -0500

    Newton's method added

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -8,10 +8,21 @@ import autodiff.optimize as opt
 def equals(a, b, tol=1e-10):
     return np.abs(a-b) <= tol
 
+
+def test_newton():
+    x, y = fwd.Variable(), fwd.Variable()
+    rosen = 100.0*(y - x**2)**2 + (1 - x)**2.0
+    newton_returned = opt.newton(rosen, {x: 0.0, y: 1.0})
+    assert equals(newton_returned[x], 1.0, tol=1e-6)
+    assert equals(newton_returned[y], 1.0, tol=1e-6)
+    
+
 def test_bfgs():
     x, y = fwd.Variable(), fwd.Variable()
     rosen = 100.0*(y - x**2)**2 + (1 - x)**2.0
     bfgs_returned = opt.bfgs(rosen, {x: 0.0, y: 1.0})
     assert equals(bfgs_returned[x], 1.0, tol=1e-6)
     assert equals(bfgs_returned[y], 1.0, tol=1e-6)
+
+
     
