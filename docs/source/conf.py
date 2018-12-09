0dc0bc67ef91db1e99524cb962bdf010895c9553
Sara Yueting Luo <yluo@hsph.harvard.edu>
2018-12-08 23:53:33 -0500

    read the doc updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,17 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.httpdomain',
+    'djangodocs',
+    'doc_extensions',
+    'sphinx_tabs.tabs',
+]
+
+autodoc_mock_imports = ["django"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
