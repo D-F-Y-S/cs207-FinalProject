32fb24c4124b084a8cd4dc500144cc4632b001c1
Yan Zhao <yaz712@g.harvard.edu>
2018-12-08 20:31:34 -0500

    test for plot

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -2,10 +2,11 @@ import pytest
 import numpy as np
 import autodiff.forward     as fwd
 import autodiff.rootfinding as rf
+import autodiff.plot as plot
 
 def testplot():
     x, y = fwd.Variable(), fwd.Variable()
     g = x**2 + y**2
-    plot_contour(g,{x: 1.0, y: 2.0},method = 'gradient_descent')
-    plot_contour(g,{x: 1.0, y: 2.0},method = 'newton')
+    plot.plot_contour(g,{x: 1.0, y: 2.0},method = 'gradient_descent')
+    plot.plot_contour(g,{x: 1.0, y: 2.0},method = 'newton')
   
