1ed810522998c629a5f85b48aa1361cc1b94a4fb
Yan Zhao <yaz712@g.harvard.edu>
2018-12-10 13:35:56 -0500

    function doc html format

--- autodiff/rootfinding.py
+++ autodiff/rootfinding.py
@@ -6,7 +6,21 @@ import warnings
 # import autodiff.forward as fwd
 
 def newton_scalar(f, init_val_dict, max_itr, tol=1e-8):
+    """
+    Newton's Method finding 0 root for a single expression 
     
+    INPUTS
+    =======
+    f: expression 
+    init_val_dict: dictionary containing initial value of variables
+    max_itr: maximum iteration before the algorithm stops
+    tol: tolerance, the minimum threshold for absolute 
+    difference of value of f from 0 for the algorithm to stop
+    
+    RETURNS
+    ========
+    variable values corresponding to the 0 point of f
+    """
     itr = 1
     val_dict = init_val_dict.copy()
     
